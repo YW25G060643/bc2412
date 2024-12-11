aa798b4923c8c06801353203b4af17bbd0c27166
Xinshun Wu <yw25g.060643@gmail.com>
2024-12-11 15:52:16 +0800

    241211

---
+++ demo helloworld/src/demoConditional.java
@@ -0,0 +1,15 @@
+public class demoConditional {
+  public static void main(String [] args){
+    //if -> check event ->event result
+    int x = 3;
+    if (x > 2){
+      System.out.println("x is > 2");
+    }
+
+    String s = "hello";
+    //check if length > 3
+    if (s.length() > 3){
+      System.out.println("s is > 3");
+    }
+  }
+}
