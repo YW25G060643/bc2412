92546561470aae296b87a8f4cfb2214d5e8d8ae7
Xinshun Wu <yw25g.060643@gmail.com>
2024-12-11 16:23:40 +0800

    241211

--- demo helloworld/src/demoConditional.java
+++ demo helloworld/src/demoConditional.java
@@ -21,5 +21,27 @@ public class demoConditional {
     if ('h' == firstChar) {
       System.out.println("s`s first number = h");
     }
+
+    if (x > 5){
+      System.out.println("x > 5");
+    }else{
+      System.out.println("x <= 5");
+    }
+
+    int y = 10;
+    //check if y is an even number
+    if (y % 2 == 0){
+      System.out.println("y is an even number");
+    }else{
+      System.out.println("y is an odd number");
+    }
+
+    //check if y > 3 and an even number
+    if (y > 3){
+      System.out.println("y > 3");
+    }
+    if (y % 2 == 0) {
+      System.out.println("y is an even number");
+    }
   }
 }
