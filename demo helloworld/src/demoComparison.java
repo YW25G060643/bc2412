befb81c65457e15936da8e8f0def70e81cd4423f
Xinshun Wu <yw25g.060643@gmail.com>
2024-12-11 12:56:29 +0800

    241211

--- demo helloworld/src/demoComparison.java
+++ demo helloworld/src/demoComparison.java
@@ -46,5 +46,24 @@ public class demoComparison {
     //and or (true and (fales or true))
     boolean result5 = age2 >=30 && (gender == 'm' || op0rnot == 'Y');
     System.out.println(result5);
+
+    //|| or (false ||false)
+    boolean result6 = age2 == 20 || gender =='m';
+    System.out.println(result6);
+
+    int x10 = 5;
+    int y10 = 10;
+    int z10 = x10 += 3 * y10;
+    System.out.println(z10);//35
+    
+    int x = 3;
+    int y = (x++ + 3) * x++;
+    System.out.println(y);//= 24
+    System.out.println(x);//= 5
+    //1. 3 + 3
+    //2. x = 4
+    //3. 6 * 4
+    //4. assignment
+    //5. x becomes 4
   }
 }
