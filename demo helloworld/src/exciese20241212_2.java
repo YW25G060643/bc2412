2aa1eed669b800bbceb0546ddc197bbecccfbf03
Xinshun Wu <yw25g.060643@gmail.com>
2024-12-12 18:13:19 +0800

    241212

--- demo helloworld/src/exciese20241212_2.java
+++ demo helloworld/src/exciese20241212_2.java
@@ -16,58 +16,58 @@ public class exciese20241212_2 {
     // The February has 29 days: (Every 4 years and the year cannot divded by
     // 100) or The year can divided by 400
     // otherwise the February should have 28 days only
-    if (month <= 0 || month > 12 || year < 0) {
+    if (month <= 0 || month > 12 || year < 1) {
       System.out.println("R U Kidding me?");
-    }
-
-    if(month == 1|| month == 3|| month == 5|| month == 7|| month == 8|| month == 10|| month == 12){
-      number_Of_DaysInMonth = 31;
-    }else if (month != 2) {
-      number_Of_DaysInMonth = 30;
     }else{
-      if (year % 400 == 0) {
-        number_Of_DaysInMonth = 29;
+      if(month == 1|| month == 3|| month == 5|| month == 7|| month == 8|| month == 10|| month == 12){
+        number_Of_DaysInMonth = 31;
+      }else if (month != 2) {
+        number_Of_DaysInMonth = 30;
       }else{
-        number_Of_DaysInMonth = 28;
+        if ((year * 100) % 400 == 0 && (year * 100) % 100 != 0) {
+          number_Of_DaysInMonth = 29;
+        }else{
+          number_Of_DaysInMonth = 28;
+        }
       }
+      
+      if (month == 1) {
+        monthOfName = "Jan";
+      }
+      if (month == 2) {
+        monthOfName = "Feb";
+      }
+      if (month == 3) {
+        monthOfName = "Mar";
+      }
+      if (month == 4) {
+        monthOfName = "Apr";
+      }
+      if (month == 5) {
+        monthOfName = "May";
+      }
+      if (month == 6) {
+        monthOfName = "Jun";
+      }
+      if (month == 7) {
+        monthOfName = "Jul";
+      }
+      if (month == 8) {
+        monthOfName = "Aug";
+      }
+      if (month == 9) {
+        monthOfName = "Sep";
+      }
+      if (month == 10) {
+        monthOfName = "Oct";
+      }
+      if (month == 11) {
+        monthOfName = "Nov";
+      }
+      if (month == 12) {
+        monthOfName = "Dec";
+      }
+      System.out.println(monthOfName + " " + year + " has " + number_Of_DaysInMonth + " days");
     }
-    
-    if (month == 1) {
-      monthOfName = "Jan";
-    }
-    if (month == 2) {
-      monthOfName = "Feb";
-    }
-    if (month == 3) {
-      monthOfName = "Mar";
-    }
-    if (month == 4) {
-      monthOfName = "Apr";
-    }
-    if (month == 5) {
-      monthOfName = "May";
-    }
-    if (month == 6) {
-      monthOfName = "Jun";
-    }
-    if (month == 7) {
-      monthOfName = "Jul";
-    }
-    if (month == 8) {
-      monthOfName = "Aug";
-    }
-    if (month == 9) {
-      monthOfName = "Sep";
-    }
-    if (month == 10) {
-      monthOfName = "Oct";
-    }
-    if (month == 11) {
-      monthOfName = "Nov";
-    }
-    if (month == 12) {
-      monthOfName = "Dec";
-    }
-    System.out.println(monthOfName + " " + year + " has " + number_Of_DaysInMonth + " days");
   }
 }
