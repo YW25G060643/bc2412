9b1f2329534731ea7d7a06126f948b522491981e
Xinshun Wu <yw25g.060643@gmail.com>
2024-12-13 14:54:52 +0800

    241213

--- demo helloworld/src/demoforloop.java
+++ demo helloworld/src/demoforloop.java
@@ -161,27 +161,17 @@ public class demoforloop {
     //**
     //***
     //****
-    for(int i = 0; i < 3; i++){
-      for(int j = 0; j < 4; j++){
-        if (i == 0 && j == 0) {
-          System.out.println('*');
-        }if (i == 0 && j == 1) {
+    int n = 4;
+    for(int i = 0; i < n; i++){
+      for(int j = 0; j < i + 1; j++){
           System.out.print('*');
-        }if (i == 0 && j == 2) {
-          System.out.println('*');
-        }if (i == 0 && j == 3) {
-          System.out.print('*');
-          System.out.print('*');
-        }if (i == 1 && j == 0) {
-          System.out.println('*');
-        }if (i == 1 && j == 1) {
-          System.out.print('*');
-          System.out.print('*');
-          System.out.print('*');
-        }if (i == 1 && j == 2) {
-          System.out.println("* 13");
-        }
       }
+      System.out.println();
     }
+
+    //    *
+    //   ***
+    //  *****
+    // *******
   }
 }
