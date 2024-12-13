1966c0c7e7090973cf7e87b4a8de3a49b8778c58
Xinshun Wu <yw25g.060643@gmail.com>
2024-12-13 16:51:27 +0800

    241213

--- demo_array/src/demo_array.java
+++ demo_array/src/demo_array.java
@@ -33,5 +33,49 @@ public class demo_array {
             Sum = Sum + arr1[i];
         }
         System.out.println(Sum);
+
+        //String array
+        String[] arr2 = new String[5];
+        arr2[0] = "abc";
+        arr2[1] = "def";
+        arr2[2] = "ghi";
+        arr2[3] = "jkl";
+        arr2[4] = "mno";
+        //targeting "abc"
+        String target = "abc";
+        for(int i = 0; i < arr2.length; i++){
+            if (target.equals(arr2[i])) {
+                System.out.println("No." + (i + 1) + " true");
+            }else{
+                System.out.println("No." + (i + 1) + " false");
+            }
+        }
+
+        //char
+        char[] arr3 = new char[]{'b', 'c', 'a'};
+        //convert char to int value and assign them to a new array
+        int[] arr4 = new int[arr3.length];
+        for(int i = 0; i < arr3.length; i++){
+            arr4[i] = arr3[i];//char -> int value
+            System.out.println(arr4[i]);
+        }
+        //find the max ASCII value in the array
+        int max = 0;
+        for(int i = 0; i < arr4.length; i++){
+            if (arr4[i] >= arr4[0] && arr4[i] >= arr4[1] && arr4[i] >= arr4[2]){
+                max = arr4[i];
+            }
+        }
+        System.out.println(max);
+
+        //find the min value in the int array
+        int[] arr5 = new int[]{9, -8, -99, 98};
+        int min = 0;
+        for(int i = 0; i < arr5.length; i++){
+            if (arr5[i] <= arr5[0] && arr5[i] <= arr5[1] && arr5[i] <= arr5[2] && arr5[i] <= arr5[3]){
+                min = arr5[i];
+            }
+        }
+        System.out.println(min);
     }
 }
