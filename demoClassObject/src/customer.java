fa571fab9b37493a99be4a429d5744dd6abc24b0
Xinshun Wu <yw25g.060643@gmail.com>
2024-12-24 16:43:52 +0800

    241224

--- demoClassObject/src/customer.java
+++ demoClassObject/src/customer.java
@@ -1,11 +1,27 @@
 public class customer {
     private long id;
     private order[] order;//reference
-    
+
+    /*1. normally customer can be created without order,
+    public customer (long id, order[] orders){
+        this.id = id
+        this.orders = orders
+    }
+    */
+
     public customer(long id){
         this.id = id;
         this.order = new order[0];
     }
+
+    /*order id = item[]
+     * totalOrderAmount by order id
+     */
+    public item[] getItem(long orderId){
+        
+    }
+     /* isVip() = 100_000 (total amount of all orders)
+     */
 
     //coustomer can place order after become creating the customer account
     public void placeOrder(order order){
